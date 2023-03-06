016aec4e60c294e50ff3765d9716a41919cc8d9f
iwataf <iwata@pickle.ne.jp>
2023-03-06 19:57:05 +0900

    フォルダ分け

--- Assets/Scripts/BaseScript/GameMain.cs
+++ Assets/Scripts/BaseScript/GameMain.cs
@@ -9,12 +9,15 @@ public class GameMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (Fader.Instance == null)
-        {
-            SceneManager.LoadSceneAsync("Boot");
-            return;
-        }
-        Fader.Instance.FadeIn(1);
+        //if (Fader.Instance == null)
+        //{
+        //    SceneManager.LoadSceneAsync("Boot");
+        //    return;
+        //}
+        //Fader.Instance.FadeIn(1,0, () =>
+        //{
+        //    // 指定したゲームのスクリプトを実行する
+        //});
     }
 
     // Update is called once per frame
